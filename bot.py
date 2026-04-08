5dd6d534a6c7e0bc974bc7773b58f4c13b7fdaea
Ekvicz <katangcz@seznam.cz>
2026-04-08 23:08:38 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -117,7 +117,7 @@ def run_web():
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     # Spuštění Flasku ve vedlejším vlákně
     Thread(target=run_web, daemon=True).start()
     
