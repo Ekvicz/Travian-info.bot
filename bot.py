9c07b22c1c7cdab80ae10faf5f0ac928dc28de57
Ekvicz <katangcz@seznam.cz>
2026-04-08 22:44:02 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ URL_MAP = 'https://ts1.x1.europe.travian.com/map.sql'
 URL_STATS = 'https://ts1.x1.europe.travian.com/statistiken.sql'
 # =============================================================
 
-app = flask(__name__)
+app = if __name__ == "__main__":
 
 @app.route('/')
 def ping():
